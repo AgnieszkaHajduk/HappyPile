9cde64a1396bb1cd54859d9664f6251d0fe4155b
HP <agnieszkawalega1996@gmail.com>
2022-12-19 12:12:22 +0100

    Finalizacja projektu, konfiguracja bazy danych na Heroku

--- src/main/java/pl/edu/wszib/happypile/HappyPileApplication.java
+++ src/main/java/pl/edu/wszib/happypile/HappyPileApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-//@PropertySource("classpath:db.properties")
 public class HappyPileApplication {
 
     public static void main(String[] args) {
